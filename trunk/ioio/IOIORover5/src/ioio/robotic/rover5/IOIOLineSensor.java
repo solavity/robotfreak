2c3102b6975ec4f94f504d712268c7bd2ff563d2
p.recktenwald@gmail.com <p.recktenwald@gmail.com@478efbe2-c4d3-11de-81ce-f10158adbdd1>
2012-06-20 19:23:30 +0000

    IOIO Rover5 initial release

---
+++ trunk/ioio/IOIORover5/src/ioio/robotic/rover5/IOIOLineSensor.java
@@ -0,0 +1,46 @@
+package ioio.robotic.rover5;
+
+import ioio.lib.api.AnalogInput;
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.IOIO;
+import ioio.lib.api.exception.ConnectionLostException;
+
+public class IOIOLineSensor {
+	private int[] ls;
+	private DigitalOutput enableLS_;
+	private AnalogInput line1_;
+	private AnalogInput line2_;
+	private AnalogInput line3_;
+	private AnalogInput line4_;
+	private AnalogInput line5_;
+	private AnalogInput line6_;
+
+	public IOIOLineSensor(IOIO ioio, int enablePin, int[] lsPins)
+			throws ConnectionLostException {
+		enableLS_ = ioio.openDigitalOutput(enablePin);
+		line1_ = ioio.openAnalogInput(lsPins[0]);
+		line2_ = ioio.openAnalogInput(lsPins[0]);
+		line3_ = ioio.openAnalogInput(lsPins[0]);
+		line4_ = ioio.openAnalogInput(lsPins[0]);
+		line5_ = ioio.openAnalogInput(lsPins[0]);
+		line6_ = ioio.openAnalogInput(lsPins[0]);
+	}
+
+	public void readLineSensor(IOIO ioio) throws ConnectionLostException {
+		enableLS_.write(false);
+		try {
+			Thread.sleep(5);
+			ls[0] = (int) (line1_.getVoltage() * 1000);
+			ls[1] = (int) (line2_.getVoltage() * 1000);
+			ls[2] = (int) (line3_.getVoltage() * 1000);
+			ls[2] = (int) (line4_.getVoltage() * 1000);
+			ls[4] = (int) (line5_.getVoltage() * 1000);
+			ls[5] = (int) (line6_.getVoltage() * 1000);
+		} catch (InterruptedException e) {
+			ioio.disconnect();
+			e.printStackTrace();
+		}
+		enableLS_.write(true);
+
+	}
+}
---
+++ trunk/ioio/IOIORover5/src/ioio/robotic/rover5/IOIOUltrasonic.java
@@ -0,0 +1,55 @@
+package ioio.robotic.rover5;
+
+import ioio.lib.api.PulseInput;
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.PulseInput.PulseMode;
+import ioio.lib.api.IOIO;
+import ioio.lib.api.exception.ConnectionLostException;
+
+public class IOIOUltrasonic {
+	private DigitalOutput trigger_;
+	private PulseInput echo_;
+	private int echoSeconds;
+	private int echoDistanceCm;
+	private int echoDistanceInch;
+
+	public IOIOUltrasonic(IOIO ioio, int echoPin, int triggerPin)
+			throws ConnectionLostException {
+		/* ultrasonic sensor */
+		echo_ = ioio.openPulseInput(echoPin, PulseMode.POSITIVE);
+		trigger_ = ioio.openDigitalOutput(triggerPin);
+	}
+
+	public int readUltrasonic(IOIO ioio) throws ConnectionLostException {
+		// read HC-SR04 ultrasonic sensor
+		try {
+			trigger_.write(false);
+			Thread.sleep(5);
+			trigger_.write(true);
+			Thread.sleep(1);
+			trigger_.write(false);
+			echoSeconds = (int) (echo_.getDuration() * 1000 * 1000);
+			setEchoDistanceCm(echoSeconds / 29 / 2);
+			setEchoDistanceInch(echoSeconds / 47 / 2);
+		} catch (InterruptedException e) {
+			ioio.disconnect();
+		}
+		return echoDistanceCm;
+	}
+
+	public void setEchoDistanceCm(int echoDistanceCm) {
+		this.echoDistanceCm = echoDistanceCm;
+	}
+
+	public int getEchoDistanceCm() {
+		return echoDistanceCm;
+	}
+
+	public void setEchoDistanceInch(int echoDistanceInch) {
+		this.echoDistanceInch = echoDistanceInch;
+	}
+
+	public int getEchoDistanceInch() {
+		return echoDistanceInch;
+	}
+}
