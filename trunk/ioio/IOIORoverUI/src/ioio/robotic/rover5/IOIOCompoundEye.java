af039487a752f1758732411048041edf177735f0
p.recktenwald@gmail.com <p.recktenwald@gmail.com@478efbe2-c4d3-11de-81ce-f10158adbdd1>
2012-07-01 21:16:45 +0000

    IOIO Rover5 User Interface for Android v4.  initial release

---
+++ trunk/ioio/IOIORoverUI/src/ioio/robotic/rover5/IOIOCompoundEye.java
@@ -0,0 +1,82 @@
+package ioio.robotic.rover5;
+
+import ioio.lib.api.AnalogInput;
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.IOIO;
+import ioio.lib.api.exception.ConnectionLostException;
+
+public class IOIOCompoundEye {
+	private DigitalOutput enableCE_;
+	private AnalogInput ceUp_;
+	private AnalogInput ceLt_;
+	private AnalogInput ceRt_;
+	private AnalogInput ceDn_;
+	private int ceUp, ceLt, ceRt, ceDn;
+	private int onUp, onLt, onRt, onDn;
+	private int ofUp, ofLt, ofRt, ofDn;
+	private int distance;
+	private int valPan, valTilt;
+	private int panScale, tiltScale;
+	private int leftright, updown;
+
+	public IOIOCompoundEye(IOIO ioio, int enablePin, int upPin, int ltPin,
+			int rtPin, int dnPin) throws ConnectionLostException {
+		enableCE_ = ioio.openDigitalOutput(18);
+		ceUp_ = ioio.openAnalogInput(39);
+		ceLt_ = ioio.openAnalogInput(40);
+		ceRt_ = ioio.openAnalogInput(42);
+		ceDn_ = ioio.openAnalogInput(41);
+	}
+
+	public void readCompoundEye(IOIO ioio) throws ConnectionLostException {
+		try {
+			// read compound eye IR sensor
+			enableCE_.write(false);
+			Thread.sleep(5);
+			onUp = (int) (ceUp_.getVoltage() * 1000);
+			onLt = (int) (ceLt_.getVoltage() * 1000);
+			onRt = (int) (ceRt_.getVoltage() * 1000);
+			onDn = (int) (ceDn_.getVoltage() * 1000);
+			enableCE_.write(true);
+			Thread.sleep(5);
+			ofUp = (int) (ceUp_.getVoltage() * 1000);
+			ofLt = (int) (ceLt_.getVoltage() * 1000);
+			ofRt = (int) (ceRt_.getVoltage() * 1000);
+			ofDn = (int) (ceDn_.getVoltage() * 1000);
+		} catch (InterruptedException e) {
+			ioio.disconnect();
+		}
+		ceUp = onUp - ofUp;
+		ceLt = onLt - ofLt;
+		ceRt = onRt - ofRt;
+		ceDn = onDn - ofDn;
+		distance = (ceUp + ceLt + ceRt + ceDn) / 4;
+		if (distance < 300) {
+			if (valPan > 1500)
+				valPan = valPan - 10;
+			if (valPan < 1500)
+				valPan = valPan + 10;
+			if (valTilt > 1500)
+				valTilt = valTilt - 10;
+			if (valTilt < 1500)
+				valTilt = valTilt + 10;
+		} else {
+			panScale = (ceLt + ceRt) / 3;
+			tiltScale = (ceUp + ceDn) / 3;
+
+			leftright = (ceLt - ceRt) * 5 / panScale;
+			valPan = valPan + leftright * 10;
+			updown = (ceDn - ceUp) * 5 / tiltScale;
+			valTilt = valTilt + updown * 10;
+
+			if (valPan < 1000)
+				valPan = 1000;
+			if (valPan > 2000)
+				valPan = 2000;
+			if (valTilt < 1000)
+				valTilt = 1000;
+			if (valTilt > 1900)
+				valTilt = 1700;
+		}
+	}
+}
