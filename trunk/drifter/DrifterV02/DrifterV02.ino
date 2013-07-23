e36596739cdc6d8d01b88de36e645bb5f9082365
p.recktenwald@gmail.com <p.recktenwald@gmail.com@478efbe2-c4d3-11de-81ce-f10158adbdd1>
2013-07-23 20:44:44 +0000

    Actual projects: MURCS, MURtle, FrankenBoB, MakeyMakey, Drifter

---
+++ trunk/drifter/DrifterV02/DrifterV02.ino
@@ -0,0 +1,297 @@
+// Drifter V0.1
+// RC Car hack by RobotFreak
+
+// I/O definitions
+#define RCinFwPin 3
+#define RCinBwPin 4
+#define RCinLtPin 7
+#define RCinRtPin 8
+
+#define DriveFwPin 5
+#define DriveBwPin 6
+#define DriveLtPin 9
+#define DriveRtPin 10
+
+#define EchoPin    11
+#define TriggerPin 12
+
+#define RCCMD_FORWARD  1
+#define RCCMD_BACKWARD 2
+#define RCCMD_LEFT     4
+#define RCCMD_RIGHT    8
+
+#define MODE_UNKNOWN   0
+#define MODE_MANUAL    1
+#define MODE_DRIFT_LT  2
+#define MODE_DRIFT_RT  3
+#define MODE_STOP      4
+#define MODE_TEST      5
+
+int mode, rccmd;
+long duration;
+long cmMsec, inMsec;
+
+int readRC(void)
+{
+  int val = 0;
+  val |= (digitalRead(RCinFwPin)) ? RCCMD_FORWARD : 0;
+  val |= (digitalRead(RCinBwPin)) ? RCCMD_BACKWARD : 0;
+  val |= (digitalRead(RCinLtPin)) ? RCCMD_LEFT : 0;
+  val |= (digitalRead(RCinRtPin)) ? RCCMD_RIGHT : 0;
+  return val;  
+}
+
+void writeDrive(int cmd)
+{
+  digitalWrite(DriveFwPin, (cmd & RCCMD_FORWARD));
+  digitalWrite(DriveBwPin, (cmd & RCCMD_BACKWARD));
+  digitalWrite(DriveLtPin, (cmd & RCCMD_LEFT));
+  digitalWrite(DriveRtPin, (cmd & RCCMD_RIGHT));
+}
+
+
+long readHCSR04()
+{
+  long pulsein;
+  // The HC-SR04 is triggered by a HIGH pulse of 10 or more microseconds.
+  digitalWrite(TriggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TriggerPin, LOW);
+  // The same pin is used to read the signal from the HC-SR04 a HIGH
+  // pulse whose duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pulsein = pulseIn(EchoPin, HIGH);
+  return pulsein;
+}
+
+long microsecondsToInches(long microseconds)
+{
+  // According to Parallax's datasheet for the PING))), there are
+  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+  // second).  This gives the distance travelled by the ping, outbound
+  // and return, so we divide by 2 to get the distance of the obstacle.
+  return microseconds / 74 / 2;
+}
+ 
+long microsecondsToCentimeters(long microseconds)
+{
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
+
+void rampSpeed(int newSpeed, int steps)
+{
+  static int actSpeed = 0;
+  int diff;
+  if (newSpeed > actSpeed) // forward accelerate
+  {
+    if ((steps > 0) && (newSpeed < (actSpeed + steps)))
+      actSpeed += steps;
+    else
+      actSpeed = newSpeed;
+  }
+  else if (newSpeed < actSpeed) // forward decelerate
+  {
+    if ((steps > 0) && (newSpeed < (actSpeed + steps)))
+      actSpeed += steps;
+    else
+      actSpeed = newSpeed;
+  }
+  else
+    actSpeed = newSpeed;
+  analogWrite(DriveFwPin, actSpeed);
+}
+
+void rampDirection(int newDirection, int steps)
+{
+  static int actDirection = 0;
+  int diff;
+  if (newDirection > actDirection) // direction accelerate
+  {
+    if ((steps > 0) && (newDirection < (actDirection + steps)))
+      actDirection += steps;
+    else
+      actDirection = newDirection;
+  }
+  else if (newDirection < actDirection) // direction deccelerate
+  {
+    if ((steps > 0) && (newDirection < (actDirection + steps)))
+      actDirection += steps;
+    else
+      actDirection = newDirection;
+  }
+  else
+    actDirection = newDirection;
+  analogWrite(DriveLtPin, actDirection);
+}
+
+void modeAuto()
+{
+  int newSpeed, newDirection;
+  duration = readHCSR04();
+  
+  cmMsec = microsecondsToCentimeters(duration);
+  inMsec = microsecondsToInches(duration);
+  
+  Serial.print("CM: ");
+  Serial.print(cmMsec);
+  Serial.print(", IN: ");
+  Serial.println(inMsec);
+  if (cmMsec > 0 && cmMsec < 100)
+  {
+    if (cmMsec > 40)
+    {
+      newDirection = 0;
+      newSpeed = 200;
+    }
+    else if (cmMsec < 10)
+    {
+      newDirection = 0;
+      newSpeed = 0;
+      rampSpeed(newSpeed, 0);
+    }
+    else
+    {
+//      if (mode == MODE_DRIFT_LT)
+        newDirection = 200;
+//      else if  (mode == MODE_DRIFT_RT)
+//        newDirection = -200;
+      newSpeed = 100;
+     }
+    rampSpeed(newSpeed, 10);
+    rampDirection(newDirection, 20);
+    delay(40);
+  }
+}
+
+void modeManual()
+{
+  rccmd = readRC();
+  if (rccmd != 0)
+  {
+    Serial.print("Cmd: ");
+    Serial.println(rccmd ,HEX);
+  }  
+  writeDrive(rccmd);
+}
+
+void modeSelect()
+{
+  rccmd = readRC();
+  if (rccmd != 0)
+  {
+    switch(rccmd)
+    {
+      case RCCMD_FORWARD:
+        mode = MODE_MANUAL;
+        Serial.println("Mode: Manu");
+      break;
+      case RCCMD_LEFT:
+        mode = MODE_DRIFT_LT;
+        Serial.println("Mode: Drift Left");
+      break;
+      case RCCMD_RIGHT:
+        mode = MODE_DRIFT_RT;
+        Serial.println("Mode: Drift Right");
+      break;
+      case RCCMD_BACKWARD:
+        mode = MODE_TEST;
+        Serial.println("Mode: Test");
+      break;
+    }
+  }  
+}
+
+void modeStop()
+{
+  writeDrive(0);
+  mode = MODE_UNKNOWN;
+}
+
+void modeTest()
+{
+  static int drive = 0;
+  static int steer = 0;
+  
+  rccmd = readRC();
+  if (rccmd != 0)
+  {
+    Serial.print("Cmd: ");
+    Serial.println(rccmd ,HEX);
+  }  
+  switch (rccmd)
+  {
+      case RCCMD_FORWARD:
+        drive += 10;
+        analogWrite(DriveFwPin, drive);
+      break;
+      case RCCMD_LEFT:
+        steer += 10;
+        analogWrite(DriveLtPin, steer);
+      break;
+      case RCCMD_RIGHT:
+        steer += 10;
+        analogWrite(DriveRtPin, steer);
+      break;
+      case RCCMD_BACKWARD:
+        drive += 10;
+        analogWrite(DriveBwPin, drive);
+      break;
+      default:
+        drive = 0;
+        steer = 0;
+        writeDrive(0);
+      break;  
+  }
+  delay(100);
+}
+
+void setup()
+{
+  
+  Serial.begin(38400);
+  Serial.println("Drifter v0.1");
+  pinMode(RCinFwPin, INPUT_PULLUP);  
+  pinMode(RCinBwPin, INPUT_PULLUP);  
+  pinMode(RCinLtPin, INPUT_PULLUP);  
+  pinMode(RCinRtPin, INPUT_PULLUP);  
+
+  pinMode(DriveFwPin, OUTPUT);  
+  pinMode(DriveBwPin, OUTPUT);  
+  pinMode(DriveLtPin, OUTPUT);  
+  pinMode(DriveRtPin, OUTPUT);  
+  
+  pinMode(EchoPin, INPUT_PULLUP);
+  pinMode(TriggerPin, OUTPUT);
+  digitalWrite(TriggerPin, LOW);
+  
+  mode = MODE_UNKNOWN;
+}
+
+void loop()
+{
+  switch(mode)
+  {
+    case MODE_UNKNOWN:
+      modeSelect();
+    break;
+    case MODE_MANUAL:
+      modeManual();
+    break;
+    case MODE_DRIFT_LT:
+      modeAuto();
+    break;
+    case MODE_DRIFT_RT:
+     modeAuto();
+    break;
+    case MODE_STOP:
+      modeStop();
+    break;   
+    case MODE_TEST:
+      modeTest();
+    break;   
+  }
+}
+
---
+++ trunk/murcs/MiniMURCS/MiniMURCS.ino
@@ -0,0 +1,412 @@
+/* 
+ * MiniMURCS - Multi Use Robot Construction System
+ *
+ * Libs used:  
+ * microM - MicroMagician Lib, written by Russell Cameron
+ * FastSPI - WS2811 RGB LEDs, written by 
+ */
+
+#include <microM.h>
+#include <Servo.h>
+#include "FastSPI_LED2.h"
+
+#define DEBUG_LEVEL 2
+
+#define WS2811Pin     12
+#define frontServoPin 11
+#define cliffLtPin    10
+#define cliffRtPin     9
+//#define triggerPin   10
+//#define echoPin       9
+#define penServoPin    3
+
+#define encoderLtPin   3
+#define encoderRtPin   6
+#define maxbotixPin    7
+
+/* global Variables */
+
+// motor control variables
+int leftSpeed,rightSpeed = 0;
+byte leftBrake, rightBrake = 0;
+int speedStep = 10;
+
+// servo control variables
+Servo frontServo;
+Servo penServo;
+int fsPos = 90;
+int fsStep = 10;
+#define penUpPos 75
+#define penDnPos 90
+
+// IR remote control variables
+int actIRCommand, lastIRCommand = 0; 
+
+// Ultrasonic sensor variables
+long distance;
+
+// Accelerometer variables
+int magnitude = 0;
+
+// Encoder  variables
+#define ENC_LT 0
+#define ENC_RT  1
+
+int encoder[2];
+int encRaw[2];
+int encMin[2];
+int encMax[2];
+int encFlag[2];
+
+/* WS2811 Definitions */
+
+#define NUM_LEDS 2 
+
+struct CRGB { 
+  byte g; 
+  byte r; 
+  byte b; 
+};
+
+struct CRGB leds[NUM_LEDS];
+
+WS2811Controller800Mhz<12> LED;
+
+/*
+void initHCSR04()
+{
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  digitalWrite(triggerPin, LOW);
+}
+
+long microsecondsToInches(long microseconds)
+{
+  // According to Parallax's datasheet for the PING))), there are
+  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+  // second).  This gives the distance travelled by the ping, outbound
+  // and return, so we divide by 2 to get the distance of the obstacle.
+  return microseconds / 74 / 2;
+}
+ 
+long microsecondsToCentimeters(long microseconds)
+{
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
+
+long readHCSR04()
+{
+  long pulsein, distcm;
+  // The HC-SR04 is triggered by a HIGH pulse of 10 or more microseconds.
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+ 
+  // The same pin is used to read the signal from the HC-SR04 a HIGH
+  // pulse whose duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pulsein = pulseIn(echoPin, HIGH);
+  distcm = microsecondsToCentimeters(pulsein);
+  if (distcm > 4 && distcm < 200)
+  {
+    Serial.print("\tDistance:");
+    Serial.println(distcm,DEC);        // use the DEC option to convert a byte to a decimal number
+  }
+  return distcm;
+}
+*/
+
+
+int readCliff()
+{
+  int lt, rt;
+  lt = digitalRead(cliffLtPin);
+  rt = digitalRead(cliffRtPin);
+#if (DEBUG_LEVEL >= 1)
+  if (lt || rt)
+  {
+    Serial.print("\tCliff ");
+    if (lt)
+    {
+      Serial.print("Lt: ");
+    }
+    if (rt)
+    {
+      Serial.print(" Rt: ");
+    }
+    Serial.println("");
+  }
+#endif  
+}
+
+int readMaxbotix()
+{
+  int dist;
+  dist = analogRead(maxbotixPin)*1000/548;
+#if (DEBUG_LEVEL >= 1)
+  Serial.print("\tDistance: ");
+  Serial.println(dist, DEC);
+#endif  
+  return dist;
+}
+
+int readIR(void)
+{
+  int irCmd = 0;
+  if(microM.ircommand>0)                         // display command from IR receiver
+  {
+    irCmd = microM.ircommand;
+#if (DEBUG_LEVEL >= 1)
+    Serial.print("\tIR command:");
+    Serial.println(microM.ircommand,DEC);        // use the DEC option to convert a byte to a decimal number
+#endif
+    microM.ircommand=0;                          // prevents display repeating the same values
+  }
+  return irCmd; 
+}
+
+void readAccel()
+{
+  microM.Impact();                       // must be included in your loop for impact detection to work
+  if(microM.magnitude>0)                 // display results of impact
+  {
+    magnitude = microM.magnitude;
+#if (DEBUG_LEVEL >= 1)
+    Serial.println(""); 
+    Serial.print("\tMagnitude:");
+    Serial.print(microM.magnitude);
+    Serial.print("\tDelta X:");
+    Serial.print(microM.deltx);
+    Serial.print("\tDelta Y:");
+    Serial.print(microM.delty);
+    Serial.print("\tDelta Z:");
+    Serial.println(microM.deltz);
+    Serial.println(""); 
+#endif
+    microM.magnitude=0;                  // prevents display repeating the same values
+  }
+}
+
+void initEncoder()
+{
+  for(int i=0; i<2; i++)
+  {
+    encoder[i] = 0;
+    encMin[i] = 10000;
+    encMax[i] = 0;
+    encFlag[i] = 0;
+  }
+}
+
+void readEncoder()
+{
+ 
+  encRaw[ENC_LT] = analogRead(encoderLtPin);
+  encRaw[ENC_RT] = analogRead(encoderRtPin);
+  for(int i=0; i<2; i++)
+  {
+    if (encRaw[i] < 400 && encFlag[i] == 1)
+    {
+      encoder[i]++;
+      encFlag[i] = 0;
+#if (DEBUG_LEVEL >= 1)
+      Serial.print("Enc: ");
+      Serial.print(i, DEC);
+      Serial.print(" ");
+      Serial.println(encoder[i], DEC);
+#endif
+    }
+    if (encRaw[i] > 450 && encFlag[i] == 0)
+    {
+      encoder[i]++;
+      encFlag[i] = 1;
+#if (DEBUG_LEVEL >= 1)
+      Serial.print("Enc: ");
+      Serial.print(i, DEC);
+      Serial.print(" ");
+      Serial.println(encoder[i], DEC);
+#endif
+    }
+    if (encRaw[i] < encMin[i]) 
+    {
+      encMin[i] = encRaw[i];
+#if (DEBUG_LEVEL >= 2)
+      Serial.print("Min: ");
+      Serial.print(i, DEC);
+      Serial.print(" ");
+      Serial.println(encMin[i], DEC);
+#endif
+    }
+    if (encRaw[i] > encMax[i]) 
+    {
+      encMax[i] = encRaw[i];
+#if (DEBUG_LEVEL >= 2)
+      Serial.print("Max: ");
+      Serial.print(i, DEC);
+      Serial.print(" ");
+      Serial.println(encMax[i], DEC);
+#endif
+    }
+  }
+}
+
+void rampMotors()
+{
+  if (leftSpeed >= 250)
+    speedStep = -10;
+  if (leftSpeed <= -250)
+    speedStep = 10;
+  if (leftSpeed >= -250 && leftSpeed <=250)
+    leftSpeed += speedStep;
+    
+  if (rightSpeed >=250)
+    speedStep = -10;
+  if (rightSpeed <= -250)
+    speedStep = 10;
+  if (rightSpeed >= -250 && rightSpeed <=250)
+    rightSpeed += speedStep;
+#if (DEBUG_LEVEL >= 1)
+  Serial.print("\tSpeed Lt: ");   
+  Serial.print(leftSpeed, DEC);
+  Serial.print(" Rt: ");   
+  Serial.println(rightSpeed, DEC);
+#endif
+}
+
+void writeLEDs(byte r, byte g, byte b)
+{
+  for(int iLed = 0; iLed < NUM_LEDS; iLed++) 
+  {
+    leds[iLed].r = r; 
+    leds[iLed].r = g; 
+    leds[iLed].r = b; 
+  }
+}
+void readSensors()
+{
+  microM.Impact();                       // must be included in your loop for impact detection to work
+  readCliff();
+//  distance = readHCSR04();
+//  distance = readMaxbotix();
+  readEncoder();
+  readAccel();                       // must be included in your loop for impact detection to work
+  
+  actIRCommand = readIR();
+  if (actIRCommand && actIRCommand != lastIRCommand)
+  {
+    // todo: update Mode of operation
+    switch (actIRCommand)
+    {
+      case 53: // >>
+        if (fsPos < 160)
+          fsPos += fsStep;
+      break;
+      case 52: // <<  
+        if (fsPos > 20)
+          fsPos -= fsStep;
+      break;
+      case 49: // |<<
+        penServo.write(penDnPos);
+      break;
+      case 50: // >>|
+        penServo.write(penUpPos);
+      break;
+      case 57:  //stop
+      case 5:
+        leftSpeed = 0;
+        rightSpeed = 0;
+      break;
+      case 2:
+        leftSpeed = 150;
+        rightSpeed = 150;
+      break;
+      case 8:
+        leftSpeed = -150;
+        rightSpeed = -150;
+      break;
+      case 4:
+        leftSpeed = -150;
+        rightSpeed = 150;
+      break;
+      case 6:
+        leftSpeed = 150;
+        rightSpeed = -150;
+      break;
+      case 1:
+        leftSpeed = 0;
+        rightSpeed = 150;
+      break;
+      case 3:
+        leftSpeed = 150;
+        rightSpeed = 0;
+      break;
+      case 7:
+        leftSpeed = -150;
+        rightSpeed = 0;
+      break;
+      case 9:
+        leftSpeed = 0;
+        rightSpeed = -150;
+      break;
+      
+    }
+    lastIRCommand = actIRCommand ;
+  }
+  else
+    lastIRCommand = 0;
+}
+
+void updateActors()
+{
+  microM.Motors(leftSpeed,rightSpeed,leftBrake,rightBrake);    // update motor controller
+  microM.Impact();                       // must be included in your loop for impact detection to work
+   frontServo.write(fsPos);
+  microM.Impact();                       // must be included in your loop for impact detection to work
+//  LED.showRGB((byte*)leds, NUM_LEDS);
+}
+
+void doProcessing()
+{
+//  rampMotors();
+}
+
+void setup() 
+{
+  Serial.begin(38400);
+  Serial.println("MiniMURCS v0.1");
+  Serial.println("resetting!");
+
+  microM.Setup(); 
+  microM.sensitivity=30;                 // if your robot vibrates or moves over a rough surface then increase this value to prevent false triggering
+  microM.devibrate=100;                  // depends on the construction of your robot. Increase the value if you get additional readings after initial impact
+
+  initEncoder();
+  LED.init();
+  memset(leds, 0,  NUM_LEDS * sizeof(struct CRGB));    
+  writeLEDs(0,128,0); // Set LEDs to green
+  LED.showRGB((byte*)leds, NUM_LEDS);
+
+  frontServo.attach(frontServoPin);
+  frontServo.write(90);
+
+  penServo.attach(penServoPin);
+  penServo.write(penDnPos);
+  delay(2000);
+  penServo.write(penUpPos);
+
+//  initHCSR04();
+
+  delay(2000);
+}
+
+
+void loop()
+{
+  readSensors();
+  doProcessing();
+  updateActors();
+}
+
